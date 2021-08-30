6bf09864a3d47fc99d9b64af79fe41e5fc3a3fbb
adreider <adreidereconomic@gmail.com>
2021-08-30 18:07:26 -0300

    first commit

---
+++ src/pages/Tour/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { View, Text } from "react-native";
+
+const Tour = () => {
+  return (
+    <View>
+      <Text>Tela Tour</Text>
+    </View>
+  )
+
+}
+
+export default Tour;
